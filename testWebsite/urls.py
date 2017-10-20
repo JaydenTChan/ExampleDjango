154162a862f4f9643c9e9b30aa8cb4750d955d64
Jayden Chan <Jayden.t.chan@gmail.com>
2017-10-20 01:52:06 -0600

    Initial Commit

---
+++ testWebsite/urls.py
@@ -0,0 +1,9 @@
+"""Music Homepage
+"""
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+	url(r'^music/', include('music.urls')),
+]
